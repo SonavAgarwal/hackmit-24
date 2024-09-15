8d0fef6b2a24d399931490a81a0bf6caf1a22197
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2024-09-14 20:05:23 -0700

    drawing

--- sophia-test/boundingbox.py
+++ sophia-test/boundingbox.py
@@ -1,12 +1,12 @@
 import numpy as np
 from PIL import Image, ImageDraw
+from shapely.geometry import Polygon
 
 RESOLUTION = 400
 IMAGE_WIDTH_IN_METERS = 50
 METERS_PER_PIXEL = IMAGE_WIDTH_IN_METERS / RESOLUTION
-bounding_box = [[-120.1060273,38.8073838],[-120.1059951,38.8072709],[-120.1058791,38.807291],[-120.1059113,38.8074039],[-120.1060273,38.8073838]]
-
 METERS_PER_LAT_DEGREE = 111320
+ZONES_IN_METERS = [5, 15, 100]
 
 def meters_per_long_degree(lat_center):
     return 111320 * np.cos(np.radians(lat_center))
@@ -32,17 +32,67 @@ def bounding_box_to_pixel_coords(bounding_box):
 
     return coords
 
+def calculate_centroid(coords):
+    # Average of the x and y coordinates of the polygon vertices
+    x_coords = [coord[0] for coord in coords]
+    y_coords = [coord[1] for coord in coords]
+    centroid_x = sum(x_coords) / len(coords)
+    centroid_y = sum(y_coords) / len(coords)
+    return (centroid_x, centroid_y)
+
+def scale_polygon(coords, buffer_in_pixels):
+    centroid = calculate_centroid(coords)
+    scaled_coords = []
+    
+    for x, y in coords:
+        # Calculate vector from centroid to the point
+        vector_x = x - centroid[0]
+        vector_y = y - centroid[1]
+        
+        # Scale the vector
+        scaled_x = centroid[0] + vector_x * (1 + buffer_in_pixels / np.sqrt(vector_x**2 + vector_y**2))
+        scaled_y = centroid[1] + vector_y * (1 + buffer_in_pixels / np.sqrt(vector_x**2 + vector_y**2))
+        
+        scaled_coords.append((int(scaled_x), int(scaled_y)))
+    
+    return scaled_coords
+
+def get_polygons_from_bounding_box(bounding_box):
+    res = []
+    polygon_coords = bounding_box_to_pixel_coords(bounding_box)
+    res.append(polygon_coords)
+    for zone in ZONES_IN_METERS:
+        scaled_polygon_coords = scale_polygon(polygon_coords, zone)
+        res.append(scaled_polygon_coords)
+    return res
+
+
+
+
 IMAGE_NAME = "50x50.png"
+bounding_box = [[-120.1060273,38.8073838],[-120.1059951,38.8072709],[-120.1058791,38.807291],[-120.1059113,38.8074039]]
+polygon_coords = bounding_box_to_pixel_coords(bounding_box)
+scaled_polygon_coords = scale_polygon(polygon_coords, 100)
+
+polygons = get_polygons_from_bounding_box(bounding_box)
+
+print("polygon_coords: ", polygon_coords)
+print("centroid: ", calculate_centroid(polygon_coords))
+print("scaled: ", scaled_polygon_coords)
+
 
 # Load the image
 image = Image.open(IMAGE_NAME)  # Replace with your image file path
 draw = ImageDraw.Draw(image)
 
-# Define the coordinates of the polygon
-polygon_coords = bounding_box_to_pixel_coords(bounding_box)
-
 # Draw the polygon on the image
-draw.polygon(polygon_coords, outline='red', fill=None)  # Change 'red' to any color you want
+# draw.polygon(polygon_coords, outline='red', fill=None)  # Change 'red' to any color you want
+# draw.polygon(scaled_polygon_coords, outline='blue', fill=None)  # Change 'red' to any color you want
+
+for polygon in polygons:   
+    draw.polygon(polygon, outline='red', fill=None)  # Change 'red' to any color you want
+
+
 
 # Save or display the result
 image.save('image_with_polygon.jpg')  # You can save the modified image
